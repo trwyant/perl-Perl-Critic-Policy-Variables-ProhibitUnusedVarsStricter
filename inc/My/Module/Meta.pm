9cc04100abbdccf90215129a3944543bb9ee4b60
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2013-06-08 20:49:16 +0000

    Initial import
    git-svn-id: file:///Users/tom/Code/svn/repos/Perl-Critic-Policy-Variables-ProhibitUnusedVarsStricter/trunk@3891 f6fd3633-1913-47c2-bad4-372d75c948ec

---
+++ inc/My/Module/Meta.pm
@@ -0,0 +1,119 @@
+package My::Module::Meta;
+
+use 5.006001;
+
+use strict;
+use warnings;
+
+use Carp;
+
+use base qw{ Exporter };
+
+our $VERSION = '0.000_01';
+
+our @EXPORT_OK = qw{
+    build_required_module_versions
+    required_module_versions
+    recommended_module_versions
+};
+
+sub required_module_versions {
+    return (
+        'base'                      => 0,
+        'strict'                    => 0,
+        'warnings'                  => 0,
+        'Perl::Critic::Document'    => 0,
+        'Perl::Critic::Policy'      => 0,
+        'Perl::Critic::Utils'       => 0,
+        'PPI::Token::Symbol'        => 0,
+        'Readonly'                  => 0,
+        'Scalar::Util'              => 0,
+    );
+}
+
+sub build_required_module_versions {
+    return (
+        'lib'       => 0,
+        'Carp'      => 0,
+    );
+}
+
+sub recommended_module_versions {
+    return (
+        'Readonly::XS'  => 0,
+        'File::Which'   => 0,
+    );
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+My::Module::Meta - Metadata for the current module
+
+=head1 SYNOPSIS
+
+ use lib qw{ inc };
+ use My::Module::Meta qw{ recommended_module_versions };
+
+=head1 DESCRIPTION
+
+This Perl module holds metadata for the current module. It is private to
+the current module.
+
+=head1 SUBROUTINES
+
+No subroutines are exported by default, but the following subroutines
+are exportable:
+
+=head2 build_required_module_versions
+
+This subroutine returns an array of the names and versions of modules
+required for the build.
+
+=head2 recommended_module_versions
+
+This subroutine returns an array of the names and versions of
+recommended modules.
+
+=head2 required_module_versions
+
+This subroutine returns an array of the names and versions of required
+modules.
+
+=head1 SUPPORT
+
+Support is by the author. Please file bug reports at
+L<http://rt.cpan.org>, or in electronic mail to the author.
+
+=head1 AUTHOR
+
+Thomas R. Wyant, III F<wyant at cpan dot org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by Thomas R. Wyant, III
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
+
+This program is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
+##############################################################################
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 72
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=72 ft=perl expandtab shiftround :
+
---
+++ xt/author/82_optional_modules.t
@@ -0,0 +1,51 @@
+#!/usr/bin/perl
+
+##############################################################################
+#      $URL: http://perlcritic.tigris.org/svn/perlcritic/trunk/distributions/Perl-Critic/xt/author/82_optional_modules.t $
+#     $Date: 2011-05-15 17:34:46 -0400 (Sun, 15 May 2011) $
+#   $Author: clonezone $
+# $Revision: 4078 $
+##############################################################################
+
+use 5.006001;
+use strict;
+use warnings;
+
+use Readonly; # So we don't barf when we hit Readonly::XS below.
+
+use lib 'inc';
+
+use My::Module::Meta qw{ recommended_module_versions };
+
+use Test::More;
+
+#-----------------------------------------------------------------------------
+
+our $VERSION = '0.000_001';
+
+#-----------------------------------------------------------------------------
+
+# Certain developers change perl installations on occasion and don't always
+# have all the optional modules installed.  Make sure that they know that they
+# don't.  :]
+
+my %module_versions = (
+    recommended_module_versions(),
+    'Test::Memory::Cycle'   => 0,
+    'Test::Without::Module' => 0,
+);
+
+plan tests => scalar keys %module_versions;
+
+foreach my $module (sort keys %module_versions) {
+    use_ok( $module, $module_versions{$module} );
+}
+
+# Local Variables:
+#   mode: cperl
+#   cperl-indent-level: 4
+#   fill-column: 78
+#   indent-tabs-mode: nil
+#   c-indentation-style: bsd
+# End:
+# ex: set ts=8 sts=4 sw=4 tw=78 ft=perl expandtab shiftround :
