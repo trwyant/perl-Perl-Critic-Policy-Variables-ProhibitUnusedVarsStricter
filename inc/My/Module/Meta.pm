27dc6056f3dfc2f6d7dd30fd84b3610d5e51ca64
Tom Wyant <wyant@cpan.org>
2017-11-13 18:44:48 -0500

    Version 0.100_01
    Set version to 0.100_01. Update Changes file for changes since v00.100.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -9,7 +9,7 @@ use Carp;
 
 use Exporter qw{ import };
 
-our $VERSION = '0.100';
+our $VERSION = '0.100_01';
 
 our @EXPORT_OK = qw{
     meta_merge
--- lib/Perl/Critic/Policy/Variables/ProhibitUnusedVarsStricter.pm
+++ lib/Perl/Critic/Policy/Variables/ProhibitUnusedVarsStricter.pm
@@ -15,7 +15,7 @@ use Perl::Critic::Utils qw< :booleans :characters hashify :severities >;
 
 use base 'Perl::Critic::Policy';
 
-our $VERSION = '0.100';
+our $VERSION = '0.100_01';
 
 #-----------------------------------------------------------------------------
 
