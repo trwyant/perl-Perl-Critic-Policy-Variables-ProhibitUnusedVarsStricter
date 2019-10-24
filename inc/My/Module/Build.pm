9cff068d649627a2a5377373c7dbf7f72c7776be
Tom Wyant <wyant@cpan.org>
2019-10-24 13:25:01 -0400

    Eliminate redirections in POD URL links

--- inc/My/Module/Build.pm
+++ inc/My/Module/Build.pm
@@ -87,7 +87,7 @@ L<Module::Build|Module::Build>.
 =head1 SUPPORT
 
 Support is by the author. Please file bug reports at
-L<http://rt.cpan.org>, or in electronic mail to the author.
+L<https://rt.cpan.org>, or in electronic mail to the author.
 
 =head1 AUTHOR
 
